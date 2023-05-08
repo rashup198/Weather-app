71ff774d56d91c808e774928da21a5b372614036
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-08 12:28:08 +0530

    updated

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 let oldTab = userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 oldTab.classList.add("current-tab");
-getfromSessionStorage();
+
 
 function switchTab(newTab) {
     if(newTab != oldTab) {
